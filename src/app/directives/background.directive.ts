a19e4deb18816df8f1ff21e2f55ebb027281c6cb
Alexandr Pavlov <fealtino@gmail.com>
2018-08-31 21:23:31 +0300

    Блок 4. Теория. Директивы

---
+++ src/app/directives/background.directive.ts
@@ -0,0 +1,11 @@
+import { Directive, ElementRef, OnInit } from '@angular/core';
+
+@Directive({
+    selector: '[appBackround]'
+})
+export class BackgroundDirective implements OnInit {
+    constructor(private element: ElementRef) {}
+    ngOnInit() {
+        this.element.nativeElement.style.backgroundColor = 'red';
+    }
+}
