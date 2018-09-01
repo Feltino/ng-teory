cacf81bf91aaffd9590ea3cf98d8624c73f110d0
Alexandr Pavlov <fealtino@gmail.com>
2018-09-01 12:17:27 +0300

    Блок 4. Теория. Директивы  Использование renderer

--- src/app/directives/background.directive.ts
+++ src/app/directives/background.directive.ts
@@ -1,11 +1,13 @@
-import { Directive, ElementRef, OnInit } from '@angular/core';
+import { Directive, ElementRef, OnInit, Renderer2 } from '@angular/core';
 
 @Directive({
     selector: '[appBackround]'
 })
 export class BackgroundDirective implements OnInit {
-    constructor(private element: ElementRef) {}
+    constructor(private element: ElementRef, private renderer: Renderer2) {}
     ngOnInit() {
-        this.element.nativeElement.style.backgroundColor = 'orange';
+        const {nativeElement} = this.element;
+        this.renderer.setStyle(nativeElement, 'background-color', 'orange');
+        this.renderer.addClass(nativeElement, 'white-text');
     }
 }
