c0f7e97e372433c2e894aa977fd9651c8342098c
Alexandr Pavlov <fealtino@gmail.com>
2018-09-01 21:34:04 +0300

    Блок 5. Теория. Пайпы Использование пайпов

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,14 +2,47 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  template: `
+    <div class="col-8 offset-lg-2">
+      <h2> {{ name }} </h2>
+      <h2> {{ name | lowercase}} </h2>
+      <h2> {{ name | uppercase}} </h2>
+      <h2> {{ name | slice:0:3}} </h2>
+      <h2> {{ name | slice:3:6 | uppercase}} </h2>
+      <hr>
+      <h2> {{ pi }} </h2>
+      <h2> {{ pi | number }} </h2>
+      <h2> {{ pi | number: '1.3-4' }} </h2>
+      <h2> {{ pi | number: '2.2-2' }} </h2>
+      <hr>
+      <h2> {{ money | currency: 'EUR': true }} </h2>
+      <hr>
+      <h2> {{ date }} </h2>
+      <h2> {{ date | date:'fullDate' }} </h2>
+      <h2> {{ date | date:'shortDate' }} </h2>
+      <h2> {{ date | date:'shortTime' }} </h2>
+      <hr>
+      <h2> {{ amount }} </h2>
+      <h2> {{ amount | percent }} </h2>
+      <hr>
+      <h2> {{ object }} </h2>
+      <h2><figure class="highlight"><pre> {{ object | json }} </pre></figure></h2>
+    </div>
+  `
 })
 export class AppComponent {
-  headerText = 'Создание своей директивы.';
-  items = [1, 2, 3, 4, 5];
-  current = 1;
-  onClick(number: number) {
-    this.current = number;
-  }
+  name = 'Feltino';
+  pi = Math.PI;
+  money = 350;
+  date = new Date();
+  amount = 0.45;
+  object = {
+    foo: 'bar',
+    baz: 'qux',
+    nested:
+      {
+        xyz: 3,
+        numbers: [1, 2, 3]
+      }
+  };
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,14 +4,10 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
-import { BackgroundDirective } from './directives/background.directive';
-import { MyDirectiveDirective } from './directives/my-directive.directive';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    BackgroundDirective,
-    MyDirectiveDirective
+    AppComponent
   ],
   imports: [
     BrowserModule,
