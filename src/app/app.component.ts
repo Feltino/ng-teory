aa8909101ac32629942963e7f093c6dadf835a68
Alexandr Pavlov <fealtino@gmail.com>
2018-09-02 14:54:32 +0300

    Блок 5. Теория. Пайпы Создание своего пайпа

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,46 +3,12 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   template: `
-    <div class="col-8 offset-lg-2">
-      <h2> {{ name }} </h2>
-      <h2> {{ name | lowercase}} </h2>
-      <h2> {{ name | uppercase}} </h2>
-      <h2> {{ name | slice:0:3}} </h2>
-      <h2> {{ name | slice:3:6 | uppercase}} </h2>
-      <hr>
-      <h2> {{ pi }} </h2>
-      <h2> {{ pi | number }} </h2>
-      <h2> {{ pi | number: '1.3-4' }} </h2>
-      <h2> {{ pi | number: '2.2-2' }} </h2>
-      <hr>
-      <h2> {{ money | currency: 'EUR': true }} </h2>
-      <hr>
-      <h2> {{ date }} </h2>
-      <h2> {{ date | date:'fullDate' }} </h2>
-      <h2> {{ date | date:'shortDate' }} </h2>
-      <h2> {{ date | date:'shortTime' }} </h2>
-      <hr>
-      <h2> {{ amount }} </h2>
-      <h2> {{ amount | percent }} </h2>
-      <hr>
-      <h2> {{ object }} </h2>
-      <h2><figure class="highlight"><pre> {{ object | json }} </pre></figure></h2>
+    <div class="col-lg-8 offset-lg-2">
+      <h2>{{ num }}</h2>
+      <h2>{{ num | appPow:3: 'грн.' }}</h2>
     </div>
   `
 })
 export class AppComponent {
-  name = 'Feltino';
-  pi = Math.PI;
-  money = 350;
-  date = new Date();
-  amount = 0.45;
-  object = {
-    foo: 'bar',
-    baz: 'qux',
-    nested:
-      {
-        xyz: 3,
-        numbers: [1, 2, 3]
-      }
-  };
+  num = 2;
 }
