ae44a24cf7e076362d3d338e947c223ca401f549
Alexandr Pavlov <fealtino@gmail.com>
2018-09-02 16:06:34 +0300

    Блок 5. Теория. Пайпы Создание динамического фильтра

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,12 +3,26 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   template: `
-    <div class="col-lg-8 offset-lg-2">
-      <h2>{{ num }}</h2>
-      <h2>{{ num | appPow:3: 'грн.' }}</h2>
+    <div class="col-lg-8 offset-lg-2 pt-4">
+    <input type="text" class="form-control" [(ngModel)]="searchCar">
+    <hr>
+    <ul class="list-group">
+      <li
+        class="list-group-item"
+        *ngFor="let car of cars | carFilter:searchCar:'name'; let i = index"
+      ><b>{{ i + 1}}</b> {{ car.name }} <i>{{ car.descr }}</i></li>
+    </ul>
     </div>
   `
 })
 export class AppComponent {
-  num = 2;
+  searchCar = '';
+  cars = [
+    {name: 'Ford', descr: 'WFM 1'},
+    {name: 'Mazda', descr: 'WFM 2'},
+    {name: 'Bently', descr: 'WFM 3'},
+    {name: 'Audi', descr: 'WFM 4'},
+    {name: 'Mercedes', descr: 'WFM 5'},
+    {name: 'BMW', descr: 'WFM 6'}
+  ]
 }
