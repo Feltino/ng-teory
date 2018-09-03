f96c71cfe19e6cbbc87d2004a7468c9ff7aef8fb
Alexandr Pavlov <fealtino@gmail.com>
2018-09-03 15:27:47 +0300

    Блок 6. Теория. Сервисы Создание сервиса

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,34 +2,28 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  template: `
-    <div class="col-lg-8 offset-lg-2 pt-4">
-    <input type="text" class="form-control" [(ngModel)]="searchCar">
-    <button class="btn btn-primary" (click)="addCar()">Добавить</button>
-    <hr>
-    <ul class="list-group">
-      <li
-        class="list-group-item"
-        *ngFor="let car of cars | carFilter:searchCar:'name'; let i = index"
-      ><b>{{ i + 1}}</b> {{ car.name }} <i>{{ car.descr }}</i></li>
-    </ul>
-    </div>
-  `
+  templateUrl: './app.component.html'
 })
 export class AppComponent {
-  searchCar = '';
   cars = [
-    {name: 'Ford', descr: 'WFM 1'},
-    {name: 'Mazda', descr: 'WFM 2'},
-    {name: 'Bently', descr: 'WFM 3'},
-    {name: 'Audi', descr: 'WFM 4'},
-    {name: 'Mercedes', descr: 'WFM 5'},
-    {name: 'BMW', descr: 'WFM 6'}
+    {
+      name: 'Ford',
+      isSold: false
+    },
+    {
+      name: 'Mazda',
+      isSold: true
+    },
+    {
+      name: 'Mercedes',
+      isSold: false
+    }
   ];
-  addCar() {
+
+  addCarToList(carName: string) {
     this.cars.push({
-      name: 'New Car',
-      descr: 'WFM'
+      name: carName,
+      isSold: false
     });
   }
 }
--- src/app/car-filter.pipe.ts
+++ src/app/car-filter.pipe.ts
@@ -1,11 +1,12 @@
 import { Pipe, PipeTransform } from '@angular/core';
 
+
+
 @Pipe({
   name: 'carFilter',
   pure: true
 })
 export class CarFilterPipe implements PipeTransform {
-
   transform(carList, searchStr: string, fieldName: string) {
     console.log('Filter start');
     if (carList.length === 0 || searchStr === '') {
